6051b71415b35481339e3c97a8785ba0a5cf4562
Anupam Mondal <anupammakaut@gmail.com>
2026-05-25 10:02:46 +0530

    fix grid typescript error

--- src/context/auth/CreateAuthContext.tsx
+++
@@ -1,7 +0,0 @@
-import { createContext } from "react"
-import type { AuthContexttype } from "../../typescript/interface/auth.interface"
-
-const AuthContext = createContext<AuthContexttype| null>(null)
-
-
-export default AuthContext;
--- src/typescript/type/auth.type.ts
+++
@@ -1,8 +0,0 @@
-export type AuthActionType =
-  | { type: "START_REGISTER" }
-  | { type: "SUCCESS_REGISTER" }
-  | { type: "FAILED_REGISTER"; payload: string | null }
-  | { type: "START_LOGIN" }
-  | { type: "SUCCESS_LOGIN"; payload: any }
-  | { type: "FAILED_LOGIN"; payload: string | null }
-  | { type: "SUCCESS_LOGOUT" };
